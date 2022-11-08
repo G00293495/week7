c541986d8e9196ab033569f4a778b1c0bad0de07
G00392495 <G00392495@atu.ie>
2022-11-08 14:03:09 +0000

    Caught error and prompted another chance to enter number

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -4,18 +4,22 @@ import java.util.Scanner;
 public class UsingTryCatch
 {
     public static void main(String[] args) {
-        try {
-            System.out.println("Please enter a number between 0-9 : ");
-            Scanner myscan = new Scanner(System.in);
-            int myNum = myscan.nextInt();
-            if (myNum <= 9 && myNum > 0) {
-                System.out.println("You entered " + myNum);
-            } else {
-                System.out.println("Not a valid number");
+        boolean restart = true;
+        while (restart) {
+        System.out.println("Please enter a number between 0-9 : ");
+        Scanner myscan = new Scanner(System.in);
+            try {
+                int myNum = myscan.nextInt();
+                if (myNum <= 9 && myNum > 0) {
+                    System.out.println("You entered " + myNum);
+                    restart = false;
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error");
             }
-        } catch (InputMismatchException e) {
-            System.out.println("Invalid. Please try again");
         }
     }
-
 }
+
